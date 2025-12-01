38120528f36631f1f1cf56dacfc85c661fa2dd28
stevenmondragon23 <smondragonflores@byupathway.edu>
2025-12-01 11:14:57 -0500

    i

--- chamber/scripts/items.mjs
+++ chamber/scripts/items.mjs
@@ -3,48 +3,48 @@ export const items = [
     "name": "Piura Main Square",
     "address": "Jr. Huancavelica, Piura 20001",
     "description": "The central square of the city, surrounded by colonial architecture and green areas.",
-    "image": "https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/PLAZA_DE_ARMAS_DE_PIURA_-_PIURA.jpg/1200px-PLAZA_DE_ARMAS_DE_PIURA_-_PIURA.webp"
+    "image": "images/piura-mainsquare.webp"
   },
   {
     "name": "Piura Cathedral",
     "address": "Jr. Huancavelica 394, Piura",
     "description": "A historic Catholic cathedral built in the 16th century with Spanish colonial style.",
-    "image": "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6c/Bas%C3%ADlica_Catedral_San_Miguel_Arc%C3%A1ngel_-_Piura.jpg/330px-Bas%C3%ADlica_Catedral_San_Miguel_Arc%C3%A1ngel_-_Piura.webp"
+    "image": "images/piura-cathedral.webp"
   },
   {
     "name": "Real Plaza Piura",
     "address": "Av. Sánchez Cerro 2345, Piura",
     "description": "Piura's busiest shopping mall, featuring stores, restaurants, and movie theaters.",
-    "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQUAECdQqTo0jqAdSnmgKQSstG7TgW1hXXMQw&s.webp"
+    "image": "images/piura-realplaza.webp"
   },
   {
     "name": "University of Piura (UDEP)",
     "address": "Av. Ramón Mugica 131, Piura",
     "description": "A well-known university campus with beautiful green areas and high academic standards.",
-    "image": "images/udep.webp"
+    "image": "images/piura-udep.webp"
   },
   {
     "name": "Miguel Cortés Children's Park",
     "address": "Av. Grau, Piura",
     "description": "A traditional family park with recreational spaces and playgrounds for children.",
-    "image": "images/park.webp"
+    "image": "images/piura-parque-miguelcortez.webp"
   },
   {
     "name": "Piura Riverwalk",
     "address": "Malecón Eguiguren, Piura",
     "description": "A tourist area ideal for walking, exercising, and enjoying the riverside view.",
-    "image": "images/riverwalk.webp"
+    "image": "images/piura-riverwalk.webp"
   },
   {
     "name": "Vicús Museum",
     "address": "Av. San Teodoro 397, Piura",
     "description": "An archaeological museum displaying artifacts from the ancient Vicús culture.",
-    "image": "images/vicus.webp"
+    "image": "images/piura-vicus.webp"
   },
   {
     "name": "Open Plaza Piura",
     "address": "Av. Andrés Avelino Cáceres 1150, Piura",
     "description": "A modern shopping center with supermarkets, restaurants, and a movie theater.",
-    "image": "images/openplaza.webp"
+    "image": "images/piura-openplaza.webp"
   }
 ];
